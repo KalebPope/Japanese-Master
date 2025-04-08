c748863b2885f930804818e3e88c0217da93fbef
Kaleb Pope <kalebpope15@gmail.com>
2025-04-08 18:27:01 +1000

    Add more fixes for scrolling

--- Frontend/src/pages/courses/immersion/Basics.tsx
+++ Frontend/src/pages/courses/immersion/Basics.tsx
@@ -2,58 +2,62 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Basics() {
-
   //Todo: put this logic into its own useHook
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
-    const preventScroll = (e: Event) => {
-    if (currentSlide === 0 || currentSlide === 2) {
-      e.preventDefault();
-    }
-  };
-
-
   const scroll = (direction: string) => {
-
     if (scrollRef.current) {
-
       if (direction == "Left" && currentSlide > 0) {
-
         scrollRef.current.scrollBy({ left: -1000, behavior: "smooth" });
 
-        setCurrentSlide(currentSlide - 1);
-
-      } else if (direction == "Right" && currentSlide < 2) {
+        setCurrentSlide((prev) => prev - 1);
+      } else if (direction == "Right" && currentSlide <= 1) {
+        setCurrentSlide((prev) => prev + 1);
 
         scrollRef.current.scrollBy({ left: 1000, behavior: "smooth" });
 
-        setCurrentSlide(currentSlide + 1);
       }
     }
   };
-
+  
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const [scrollX, setScroll] = useState<number>(window.scrollX);
+  
   useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    console.log(windowWidth);
 
-    if (scrollRef.current) {
-
-      scrollRef.current.scrollLeft = 1000;
-
+    const handleScroll = () => {
+      setScroll(window.scrollX)
     }
 
-  }, []);
+    console.log(scrollX);
+    
+    window.addEventListener("resize", handleResize);
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [windowWidth, scrollX]);
 
   return (
     <>
       <div className="flex items-center relative">
         <div
           ref={scrollRef}
-          className="relative flex w-full gap-6 snap-x snap-center snap-mandatory overflow-x-auto scroll-hide h-calc justify-items-center items-center">
-          <div className="shrink-0 snap-center">
-            <span className="w-250 h-150 shrink-0 flex items-center justify-center" />
-          </div>
-          <div className="shrink-0 snap-center relative">
+          className={`relative flex gap-6 snap-x w-full snap-center snap-mandatory overflow-x-auto scroll-hide h-calc justify-items-center items-center`}>
+          <div
+            className="shrink-0 snap-center relative"
+            style={{
+              paddingLeft: `${windowWidth / 2 - 506}px`,
+            }}>
             <Link
               to="introduction"
               className="bg-custom bg-cover w-250 h-150 flex items-center justify-center">
@@ -65,13 +69,14 @@ export default function Basics() {
               Introduction
             </a>
           </div>
-          <div className="shrink-0 snap-center">
-            <a className="bg-custom bg-cover w-250 h-150 flex items-center justify-center">
+          <div className="shrink-0 snap-center"
+          style={{
+            paddingRight: `${windowWidth / 2 - 506}px`,
+          }}>
+            <a
+              className="bg-custom bg-cover w-250 h-150 flex items-center justify-center">
               Introduction
             </a>
-          </div>
-          <div className="shrink-0 snap-center">
-            <span className="w-250 h-150 shrink-0 flex items-center justify-center" />
           </div>
         </div>
         <button
